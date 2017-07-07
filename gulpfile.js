f97961fc9b82327b928f71398a13700ed5483874
Sven van Steenis <sven.vsteenis@gmail.com>
2017-07-07 10:23:19 +0200

    Project restructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,50 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+'use strict';
 
-gulp.task('run', function(){
+// Dependencies
+const   gulp            = require('gulp'),
+        browserSync     = require('browser-sync'),
+        sass            = require('gulp-sass'),
+        autoprefixer    = require('gulp-autoprefixer'),
+        cssnano         = require('gulp-cssnano'),
+        rename          = require('gulp-rename');
+
+
+// Watch for changes & update browser
+gulp.task('watch', ['browserSync', 'sass'], function(){ 
+    gulp.watch('assets/scss/**/*.scss', ['minify-css']);
+    gulp.watch('assets/js/**/*.js', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+});
+
+// Basic local server
+gulp.task('browserSync', function(){
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: ''
         },
         browser: 'chrome'
     });
-
-    gulp.watch('./index.html').on('change', browserSync.reload);
-    gulp.watch('./js/core.js').on('change', browserSync.reload);
-
-    gulp.watch('./css/*', function() {
-        gulp.src('./css/*')
-            .pipe(browserSync.stream());
-    });
-
 });
 
-gulp.task('default', function() {
+// Compiles sass to css, works with browsersync
+gulp.task('sass', function() {
+    return gulp.src('/assets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('/assets/css/'))
+        .pipe(browserSync.reload({
+           stream: true 
+        }));
+});
+
+// Minifies CSS
+gulp.task('minify-css', ['sass'], function(){
+    return gulp.src('/assets/css/main.css')
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('/assets/css/'));
+});
+
+gulp.task('default', ['watch'], function() {
 
 });
