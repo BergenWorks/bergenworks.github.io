afc82abbcbc1b65add9effae4a5087b60fa1f4ab
Sven van Steenis <sven.vsteenis@gmail.com>
2017-07-10 15:21:03 +0200

    Changed navigation to new branding

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,11 +1,25 @@
 // Animated scroll function for navigation
-$(".nav-link, .intro-btn").click(function(e){
+$(".nav-link, .logo-link, .intro-btn").click(function(e){
     e.preventDefault();
     var target = $(this).attr('href');
     
     $("html, body").animate({
         scrollTop: $(target).offset().top
     }, 800)
+});
+
+var waypoint = new Waypoint({
+    element: document.querySelector('#about'),
+    handler: function(direction) {
+        var nav = $(".nav-compact");
+
+        if(direction === "down") {
+            nav.stop().fadeIn(300);
+        } else if(direction === "up") {
+            nav.stop().fadeOut(300);         
+        }
+    },
+    offset: '50%'
 });
 
 // Google maps
